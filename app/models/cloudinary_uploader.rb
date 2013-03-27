99f385ff5e86c4a1ad4f265de790c365d04d3083
Thach Chau <rog.kane@gmail.com>
2013-03-27 18:57:04 +0700

    - Move uploader outside to be an independent class - Bump to version 0.0.5

---
+++ app/models/cloudinary_uploader.rb
@@ -0,0 +1,30 @@
+class CloudinaryUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Compatibility::Paperclip
+  include Cloudinary::CarrierWave
+
+  process :convert => 'png'
+  # Spree looks in attachment#errors, so just delegate to model#errors
+  delegate :errors, :to => :model
+
+  # Match the path defined in Spree::Image
+  def paperclip_path
+    "assets/products/:id/:style/:basename.:extension"
+  end
+
+  # These are the versions defined in Spree::Image
+  version :mini do
+    process :resize_to_fit => [48, 48]
+  end
+
+  version :small do
+    process :resize_to_fit => [100, 100]
+  end
+
+  version :product do
+    process :resize_to_fit => [240, 240]
+  end
+
+  version :large do
+    process :resize_to_fit => [600, 600]
+  end
+end
