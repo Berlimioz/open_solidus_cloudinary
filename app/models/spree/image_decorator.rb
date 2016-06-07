45311345192646232e40b82c9cefcff93fd7f00b
Berlimioz <berlimioz@gmail.com>
2016-06-07 18:11:33 +0200

    add solidus front end compatibility" e

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -6,6 +6,15 @@ Spree::Image.class_eval do
   def prepare_for_destroy; end
   def destroy_attached_files; end
 
+  # For solidus front end compatibility
+  def attachment(version=nil)
+    if version.nil?
+      super()
+    else
+      super().url(version)
+    end
+  end
+
   # Get rid of Paperclip validation
   def attachment_file_name
     "not_blank"
